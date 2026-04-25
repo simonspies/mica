cc7af5a5a90597b2413c63a24c40501585bee97a
Simon Spies <simonspies@icloud.com>
2026-04-25 19:33:34 +0100

    rename Heap.fresh_not_lookup to Heap.lookup_fresh

--- Mica/TinyML/Heap.lean
+++ Mica/TinyML/Heap.lean
@@ -21,7 +21,7 @@ theorem lookup_update_ne (μ : Heap) (l l' : Runtime.Location) (v : Runtime.Val)
     (μ.update l v).lookup l' = μ.lookup l' := by
   simp [lookup, update, Finmap.lookup_insert_of_ne _ h.symm]
 
-theorem fresh_not_lookup (μ : Heap) (l : Runtime.Location) (h : μ.Fresh l) :
+theorem lookup_fresh (μ : Heap) (l : Runtime.Location) (h : μ.Fresh l) :
     μ.lookup l = none := by
   simp [Fresh, dom, lookup] at *
   exact Finmap.lookup_eq_none.mpr h
