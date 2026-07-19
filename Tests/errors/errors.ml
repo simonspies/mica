5208f826ae202cc08b0208ebbbad6c5e85bef449
Simon Spies <simonspies@icloud.com>
2026-07-19 14:07:02 +0100

    shadowing for builtins

--- Tests/errors/errors.ml
+++ Tests/errors/errors.ml
@@ -17,3 +17,33 @@ let checked_sub (a : int) (b : int) : int =
 let _ = assert (checked_pos 5 = 5)
 
 let _ = assert (checked_sub 7 2 = 5)
+
+(* Local bindings shadow bare prelude primitives. *)
+let shadow_failwith (n : int) : int =
+  let failwith = n + 1 in
+  failwith
+[@@spec fun n ->
+  ret (fun v ->
+    assert (v = n + 1))];;
+
+let shadow_argument (failwith : int) : int = failwith
+[@@spec fun failwith ->
+  ret (fun v ->
+    assert (v = failwith))];;
+
+(* Top-level bindings shadow the opened prelude in later declarations. *)
+let invalid_arg (x : int) : int = x + 2
+[@@spec fun x ->
+  ret (fun v ->
+    assert (v = x + 2))];;
+
+let shadow_invalid_arg (n : int) : int = invalid_arg n
+[@@spec fun n ->
+  ret (fun v ->
+    assert (v = n + 2))];;
+
+let _ = assert (shadow_failwith 4 = 5)
+
+let _ = assert (shadow_argument 4 = 4)
+
+let _ = assert (shadow_invalid_arg 4 = 6)
